635dc069f129e4eab6dfacd68f4583dc650d5296
iagora <iago@passos.eng.br>
2024-06-08 20:11:47 -0300

    multiple overlaps single

--- bin/fiddle.ml
+++ bin/fiddle.ml
@@ -5,37 +5,7 @@ let list_algorithms_command =
   Command.basic ~summary:"list all supported hash algorithms"
     Command.Param.(return (fun () -> list_algorithms ()))
 
-let process_cpf_command =
-  Command.basic ~summary:"calculate check digits and hash a single CPF number"
-    (let%map_open.Command cpf = anon ("cpf" %: string)
-     and hash_algorithm =
-       flag "-hash" ~aliases:[ "-h" ]
-         (optional_with_default "sha256" string)
-         ~doc:
-           "HASH specify the hash algorithm. Use --list-algorithms for a full \
-            list"
-     and mac_algorithm =
-       flag "-mac" ~aliases:[ "-m" ] (optional string)
-         ~doc:
-           "MAC specify the MAC algorithm. Use --list-algorithms for a full \
-            list. Secret key must be in FIDDLE_SECRET_KEY env var"
-     and digest_length =
-       flag "-length" ~aliases:[ "-l" ]
-         (optional_with_default 512 int)
-         ~doc:
-           "LENGTH specify the digest length in bits for algorithms that have \
-            varying length output"
-     in
-     fun () ->
-       match (hash_algorithm, mac_algorithm) with
-       | hash, None ->
-           let number = Int.of_string cpf in
-           hash_a_cpf hash digest_length number
-       | _, Some mac ->
-           let number = Int.of_string cpf in
-           mac_a_cpf mac digest_length number)
-
-let process_multiple_cpfs_command =
+let process_cpfs_command =
   Command.basic ~summary:"process multiple CPFs from command line arguments"
     (let%map_open.Command cpfs = anon (sequence ("cpf" %: string))
      and hash_algorithm =
@@ -100,9 +70,8 @@ let process_stdin =
 let () =
   Command.group ~summary:"fiddle"
     [
-      ("single", process_cpf_command);
       ("stdin", process_stdin);
-      ("multiple", process_multiple_cpfs_command);
+      ("process", process_cpfs_command);
       ("list-algorithms", list_algorithms_command);
     ]
   |> Command_unix.run
