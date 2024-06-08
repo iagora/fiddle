c2aa1c3f89ea05dd3abb0054471cbce63c38c364
iagora <iago@passos.eng.br>
2024-06-08 17:33:05 -0300

    formatting

--- bin/fiddle.ml
+++ bin/fiddle.ml
@@ -2,82 +2,96 @@ open Lib
 open Core
 
 let list_algorithms_command =
-  Command.basic
-    ~summary:"list all supported hash algorithms"
-    Command.Param.(
-        return (fun () -> list_algorithms ())
-    )
+  Command.basic ~summary:"list all supported hash algorithms"
+    Command.Param.(return (fun () -> list_algorithms ()))
 
 let process_cpf_command =
-  Command.basic
-    ~summary:"calculate check digits and hash a single CPF number"
-    (
-      let%map_open.Command
-        cpf = anon ("cpf" %: string)
-        and hash_algorithm = flag "-hash" (optional_with_default "sha256" string)
-                              ~doc:"specify the hash algorithm. Use --list-algorithms for a full list"
-        and mac_algorithm = flag "-mac" (optional string)
-                              ~doc: "specify the MAC algorithm. Use --list-algorithms for a full list. Secret key must be in FIDDLE_SECRET_KEY env var"
-        and digest_length = flag "-length" (optional_with_default 512 int)
-                              ~doc:"specify the digest length in bits for algorithms that have varying length output"
-      in
-      fun () ->
-          match hash_algorithm, mac_algorithm with
-          | hash, None -> 
-                  let number = Int.of_string cpf in
-                  hash_a_cpf hash digest_length number 
-          | _, Some mac ->
-                  let number = Int.of_string cpf in
-                  mac_a_cpf mac digest_length number 
-    )
+  Command.basic ~summary:"calculate check digits and hash a single CPF number"
+    (let%map_open.Command cpf = anon ("cpf" %: string)
+     and hash_algorithm =
+       flag "-hash"
+         (optional_with_default "sha256" string)
+         ~doc:
+           "specify the hash algorithm. Use --list-algorithms for a full list"
+     and mac_algorithm =
+       flag "-mac" (optional string)
+         ~doc:
+           "specify the MAC algorithm. Use --list-algorithms for a full list. \
+            Secret key must be in FIDDLE_SECRET_KEY env var"
+     and digest_length =
+       flag "-length"
+         (optional_with_default 512 int)
+         ~doc:
+           "specify the digest length in bits for algorithms that have varying \
+            length output"
+     in
+     fun () ->
+       match (hash_algorithm, mac_algorithm) with
+       | hash, None ->
+           let number = Int.of_string cpf in
+           hash_a_cpf hash digest_length number
+       | _, Some mac ->
+           let number = Int.of_string cpf in
+           mac_a_cpf mac digest_length number)
 
 let process_multiple_cpfs_command =
-  Command.basic
-    ~summary:"process multiple CPFs from command line arguments"
-    (
-      let%map_open.Command
-        cpfs = anon (sequence ("cpf" %: string))
-        and hash_algorithm = flag "-hash" (optional_with_default "sha256" string)
-                              ~doc:"specify the hash algorithm. Use --list-algorithms for a full list"
-        and mac_algorithm = flag "-mac" (optional string)
-                              ~doc: "specify the MAC algorithm. Use --list-algorithms for a full list. Secret key must be in FIDDLE_SECRET_KEY env var"
-        and digest_length = flag "-length" (optional_with_default 512 int)
-                              ~doc:"specify the digest length in bits for algorithms that have varying length output"
-      in
-      fun () ->
-          let f = match hash_algorithm, mac_algorithm with
-          | hash, None -> hash_a_cpf hash digest_length 
-          | _, Some mac -> mac_a_cpf mac digest_length
-          in
-          List.iter cpfs ~f:(fun cpf ->
-            let number = Int.of_string cpf in
-              f number
-        )
-    )
+  Command.basic ~summary:"process multiple CPFs from command line arguments"
+    (let%map_open.Command cpfs = anon (sequence ("cpf" %: string))
+     and hash_algorithm =
+       flag "-hash"
+         (optional_with_default "sha256" string)
+         ~doc:
+           "specify the hash algorithm. Use --list-algorithms for a full list"
+     and mac_algorithm =
+       flag "-mac" (optional string)
+         ~doc:
+           "specify the MAC algorithm. Use --list-algorithms for a full list. \
+            Secret key must be in FIDDLE_SECRET_KEY env var"
+     and digest_length =
+       flag "-length"
+         (optional_with_default 512 int)
+         ~doc:
+           "specify the digest length in bits for algorithms that have varying \
+            length output"
+     in
+     fun () ->
+       let f =
+         match (hash_algorithm, mac_algorithm) with
+         | hash, None -> hash_a_cpf hash digest_length
+         | _, Some mac -> mac_a_cpf mac digest_length
+       in
+       List.iter cpfs ~f:(fun cpf ->
+           let number = Int.of_string cpf in
+           f number))
 
 let process_stdin =
-  Command.basic
-    ~summary:"process CPFs straight from stdin"
-    (
-      let%map_open.Command
-        hash_algorithm = flag "-hash" (optional_with_default "sha256" string)
-                              ~doc:"specify the hash algorithm. Use --list-algorithms for a full list."
-        and mac_algorithm = flag "-mac" (optional string)
-                              ~doc: "specify the MAC algorithm. Use --list-algorithms for a full list. Secret key must be in FIDDLE_SECRET_KEY env var"
-        and digest_length = flag "-length" (optional_with_default 512 int)
-                              ~doc:"specify the digest length in bits for algorithms that have varying length output"
-      in
-      fun () -> (
-          let f = match hash_algorithm, mac_algorithm with
-          | hash, None -> hash_a_cpf hash digest_length 
-          | _, Some mac -> mac_a_cpf mac digest_length
-          in
-          In_channel.fold_lines In_channel.stdin ~init:() ~f:(fun () line ->
-          let number = Int.of_string line in
-          f number
-       )
-     )
-    )
+  Command.basic ~summary:"process CPFs straight from stdin"
+    (let%map_open.Command hash_algorithm =
+       flag "-hash"
+         (optional_with_default "sha256" string)
+         ~doc:
+           "specify the hash algorithm. Use --list-algorithms for a full list."
+     and mac_algorithm =
+       flag "-mac" (optional string)
+         ~doc:
+           "specify the MAC algorithm. Use --list-algorithms for a full list. \
+            Secret key must be in FIDDLE_SECRET_KEY env var"
+     and digest_length =
+       flag "-length"
+         (optional_with_default 512 int)
+         ~doc:
+           "specify the digest length in bits for algorithms that have varying \
+            length output"
+     in
+     fun () ->
+       let f =
+         match (hash_algorithm, mac_algorithm) with
+         | hash, None -> hash_a_cpf hash digest_length
+         | _, Some mac -> mac_a_cpf mac digest_length
+       in
+       In_channel.fold_lines In_channel.stdin ~init:() ~f:(fun () line ->
+           let number = Int.of_string line in
+           f number))
 
 (* Entry point of the program *)
 let () =
--- lib/lib.ml
+++ lib/lib.ml
@@ -3,47 +3,47 @@ open Stdio
 open Cryptokit
 
 let list_algorithms () =
-  let hash_algorithms = 
-      [
-          "Hash algorithms:";
-          "";
-          "\tsha3";
-          "\tkeccak";
-          "\tsha2";
-          "\tsha224";
-          "\tsha256";
-          "\tsha384";
-          "\tsha512";
-          "\tblake2b";
-          "\tblake2b512";
-          "\tblake2s";
-          "\tblake2s256";
-          "\tblake3";
-          "\tblake3_256";
-          "\tripemd160";
-          "\tsha1";
-          "\tmd5";
-       ]
+  let hash_algorithms =
+    [
+      "Hash algorithms:";
+      "";
+      "\tsha3";
+      "\tkeccak";
+      "\tsha2";
+      "\tsha224";
+      "\tsha256";
+      "\tsha384";
+      "\tsha512";
+      "\tblake2b";
+      "\tblake2b512";
+      "\tblake2s";
+      "\tblake2s256";
+      "\tblake3";
+      "\tblake3_256";
+      "\tripemd160";
+      "\tsha1";
+      "\tmd5";
+    ]
   in
   let mac_algorithms =
-      [
-          "MAC algorithms:";
-          "";
-          "\tsha256";
-          "\tsha384";
-          "\tsha512";
-          "\tblake2b";
-          "\tblake2b512";
-          "\tblake2s";
-          "\tblake2s256";
-          "\tblake3";
-          "\tblake3_256";
-          "\tripemd160";
-          "\tsiphash";
-          "\tsiphash128";
-          "\tsha1";
-          "\tmd5";
-      ]
+    [
+      "MAC algorithms:";
+      "";
+      "\tsha256";
+      "\tsha384";
+      "\tsha512";
+      "\tblake2b";
+      "\tblake2b512";
+      "\tblake2s";
+      "\tblake2s256";
+      "\tblake3";
+      "\tblake3_256";
+      "\tripemd160";
+      "\tsiphash";
+      "\tsiphash128";
+      "\tsha1";
+      "\tmd5";
+    ]
   in
   List.iter ~f:(printf "%s\n") hash_algorithms;
   printf "\n";
@@ -53,18 +53,22 @@ let list_algorithms () =
 let calculate_cpf_digits cpf_base =
   (* Helper to calculate a single digit based on the cpf array and the weights *)
   let calc_digit weights cpf_numbers =
-    List.fold2_exn cpf_numbers weights ~init:0 ~f:(fun acc x weight -> acc + x * weight)
-    |> fun sum -> let remainder = sum % 11 in
-                 if remainder < 2 then 0 else 11 - remainder
+    List.fold2_exn cpf_numbers weights ~init:0 ~f:(fun acc x weight ->
+        acc + (x * weight))
+    |> fun sum ->
+    let remainder = sum % 11 in
+    if remainder < 2 then 0 else 11 - remainder
   in
   (* Weights for the first and second check digits *)
-  let weights_first = [10; 9; 8; 7; 6; 5; 4; 3; 2] in
-  let weights_second = [11; 10; 9; 8; 7; 6; 5; 4; 3; 2] in
+  let weights_first = [ 10; 9; 8; 7; 6; 5; 4; 3; 2 ] in
+  let weights_second = [ 11; 10; 9; 8; 7; 6; 5; 4; 3; 2 ] in
   (* Calculate the first check digit *)
   let first_digit = calc_digit weights_first cpf_base in
   (* Calculate the second check digit *)
-  let complete_cpf = cpf_base @ [first_digit] in  (* Include first check digit in the list *)
-  let second_digit = calc_digit weights_second complete_cpf in  (* Now using complete_cpf *)
+  let complete_cpf = cpf_base @ [ first_digit ] in
+  (* Include first check digit in the list *)
+  let second_digit = calc_digit weights_second complete_cpf in
+  (* Now using complete_cpf *)
   (first_digit, second_digit)
 
 (* Convert integer list to CPF string *)
@@ -73,31 +77,33 @@ let cpf_list_to_string cpf_digits (d1, d2) =
   |> fun base_str -> Printf.sprintf "%s-%d%d" base_str d1 d2
 
 (* Hash the CPF using Blake2b and print both hex and base64 encodings *)
-let hash_and_print cpf_str hash_algorithm digest_length  =
+let hash_and_print cpf_str hash_algorithm digest_length =
   let hash_function =
-      match hash_algorithm with
-      | "sha3" -> Hash.sha3 digest_length
-      | "keccak" -> Hash.keccak digest_length
-      | "sha2" -> Hash.sha2 digest_length
-      | "sha224" -> Hash.sha224 ()
-      | "sha256" -> Hash.sha256 ()
-      | "sha384" -> Hash.sha384 ()
-      | "sha512" -> Hash.sha512 ()
-      | "blake2b" -> Hash.blake2b digest_length
-      | "blake2b512" -> Hash.blake2b512 ()
-      | "blake2s" -> Hash.blake2s digest_length
-      | "blake2s256" -> Hash.blake2s256 ()
-      | "blake3" -> Hash.blake3 digest_length
-      | "blake3_256" -> Hash.blake3_256 ()
-      | "ripemd160" -> Hash.ripemd160 ()
-      | "sha1" -> Hash.sha1 ()
-      | "md5" -> Hash.md5 ()
-      | _ -> failwith "Unsupported hash algorithm"
+    match hash_algorithm with
+    | "sha3" -> Hash.sha3 digest_length
+    | "keccak" -> Hash.keccak digest_length
+    | "sha2" -> Hash.sha2 digest_length
+    | "sha224" -> Hash.sha224 ()
+    | "sha256" -> Hash.sha256 ()
+    | "sha384" -> Hash.sha384 ()
+    | "sha512" -> Hash.sha512 ()
+    | "blake2b" -> Hash.blake2b digest_length
+    | "blake2b512" -> Hash.blake2b512 ()
+    | "blake2s" -> Hash.blake2s digest_length
+    | "blake2s256" -> Hash.blake2s256 ()
+    | "blake3" -> Hash.blake3 digest_length
+    | "blake3_256" -> Hash.blake3_256 ()
+    | "ripemd160" -> Hash.ripemd160 ()
+    | "sha1" -> Hash.sha1 ()
+    | "md5" -> Hash.md5 ()
+    | _ -> failwith "Unsupported hash algorithm"
   in
-  let digest = hash_function#add_string cpf_str; hash_function#result in
+  let digest =
+    hash_function#add_string cpf_str;
+    hash_function#result
+  in
   let hex_encoded = transform_string (Hexa.encode ()) digest in
-  printf "%s\t%s\n" cpf_str hex_encoded 
-
+  printf "%s\t%s\n" cpf_str hex_encoded
 
 let mac_and_print data algorithm length =
   let secret_key =
@@ -123,29 +129,29 @@ let mac_and_print data algorithm length =
     | "siphash128" -> MAC.siphash128 secret_key
     | _ -> failwith "Unsupported MAC algorithm"
   in
-  let result = mac#add_string data; mac#result in
+  let result =
+    mac#add_string data;
+    mac#result
+  in
   let hex_encoded = transform_string (Hexa.encode ()) result in
   printf "%s\t%s\n" data hex_encoded
 
-
 let hash_a_cpf hash_algorithm digest_length number =
   let int_to_cpf_array n =
-    Printf.sprintf "%09d" n
-    |> String.to_list
+    Printf.sprintf "%09d" n |> String.to_list
     |> List.map ~f:(fun c -> Char.to_int c - Char.to_int '0')
   in
   let cpf_base = int_to_cpf_array number in
   let digits = calculate_cpf_digits cpf_base in
   let cpf_str = cpf_list_to_string cpf_base digits in
-    hash_and_print cpf_str hash_algorithm digest_length
+  hash_and_print cpf_str hash_algorithm digest_length
 
 let mac_a_cpf mac_algorithm digest_length number =
   let int_to_cpf_array n =
-    Printf.sprintf "%09d" n
-    |> String.to_list
+    Printf.sprintf "%09d" n |> String.to_list
     |> List.map ~f:(fun c -> Char.to_int c - Char.to_int '0')
   in
   let cpf_base = int_to_cpf_array number in
   let digits = calculate_cpf_digits cpf_base in
   let cpf_str = cpf_list_to_string cpf_base digits in
-    mac_and_print cpf_str mac_algorithm digest_length
+  mac_and_print cpf_str mac_algorithm digest_length
