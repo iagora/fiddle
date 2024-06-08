bdb16b9889e3ab5dbf89bb3389ee50da735a307f
iagora <iago@passos.eng.br>
2024-06-08 17:42:32 -0300

    aliases

--- bin/fiddle.ml
+++ bin/fiddle.ml
@@ -9,17 +9,17 @@ let process_cpf_command =
   Command.basic ~summary:"calculate check digits and hash a single CPF number"
     (let%map_open.Command cpf = anon ("cpf" %: string)
      and hash_algorithm =
-       flag "-hash"
+       flag "-hash" ~aliases:[ "-h" ]
          (optional_with_default "sha256" string)
          ~doc:
            "specify the hash algorithm. Use --list-algorithms for a full list"
      and mac_algorithm =
-       flag "-mac" (optional string)
+       flag "-mac" ~aliases:[ "-m" ] (optional string)
          ~doc:
            "specify the MAC algorithm. Use --list-algorithms for a full list. \
             Secret key must be in FIDDLE_SECRET_KEY env var"
      and digest_length =
-       flag "-length"
+       flag "-length" ~aliases:[ "-l" ]
          (optional_with_default 512 int)
          ~doc:
            "specify the digest length in bits for algorithms that have varying \
@@ -34,50 +34,20 @@ let process_cpf_command =
            let number = Int.of_string cpf in
            mac_a_cpf mac digest_length number)
 
-let process_multiple_cpfs_command =
-  Command.basic ~summary:"process multiple CPFs from command line arguments"
-    (let%map_open.Command cpfs = anon (sequence ("cpf" %: string))
-     and hash_algorithm =
-       flag "-hash"
-         (optional_with_default "sha256" string)
-         ~doc:
-           "specify the hash algorithm. Use --list-algorithms for a full list"
-     and mac_algorithm =
-       flag "-mac" (optional string)
-         ~doc:
-           "specify the MAC algorithm. Use --list-algorithms for a full list. \
-            Secret key must be in FIDDLE_SECRET_KEY env var"
-     and digest_length =
-       flag "-length"
-         (optional_with_default 512 int)
-         ~doc:
-           "specify the digest length in bits for algorithms that have varying \
-            length output"
-     in
-     fun () ->
-       let f =
-         match (hash_algorithm, mac_algorithm) with
-         | hash, None -> hash_a_cpf hash digest_length
-         | _, Some mac -> mac_a_cpf mac digest_length
-       in
-       List.iter cpfs ~f:(fun cpf ->
-           let number = Int.of_string cpf in
-           f number))
-
 let process_stdin =
   Command.basic ~summary:"process CPFs straight from stdin"
     (let%map_open.Command hash_algorithm =
-       flag "-hash"
+       flag "-hash" ~aliases:[ "-h" ]
          (optional_with_default "sha256" string)
          ~doc:
            "specify the hash algorithm. Use --list-algorithms for a full list."
      and mac_algorithm =
-       flag "-mac" (optional string)
+       flag "-mac" ~aliases:[ "-m" ] (optional string)
          ~doc:
            "specify the MAC algorithm. Use --list-algorithms for a full list. \
             Secret key must be in FIDDLE_SECRET_KEY env var"
      and digest_length =
-       flag "-length"
+       flag "-length" ~aliases:[ "-h" ]
          (optional_with_default 512 int)
          ~doc:
            "specify the digest length in bits for algorithms that have varying \
@@ -98,7 +68,6 @@ let () =
   Command.group ~summary:"fiddle"
     [
       ("single", process_cpf_command);
-      ("multiple", process_multiple_cpfs_command);
       ("stdin", process_stdin);
       ("list-algorithms", list_algorithms_command);
     ]
